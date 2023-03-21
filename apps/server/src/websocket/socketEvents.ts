2236244de9b709ee063c33e5d4c72db523b13744
Dimas Paiva <Dimas.Paiva@ctw.bmwgroup.com>
2023-03-21 04:49:06 +0000

    feat: add a salutation message and temporary response

--- apps/server/src/websocket/socketEvents.ts
+++ apps/server/src/websocket/socketEvents.ts
@@ -2,6 +2,14 @@ import { Server as IOServer } from 'socket.io'
 
 export const addEventsListeners = (websocket: IOServer): void => {
   websocket.on('connection', (socket) => {
-    socket.emit('connected!')
+    const username = socket.handshake.auth.name
+
+    socket.emit('server message', `Hi ${username}! Welcome to reactive chat!`)
+
+    socket.on('message', (message: string) => {
+      console.log('received a message', message)
+      socket
+        .emit('server message', 'automatic message, to-do implement decision tree')
+    })
   })
 }
